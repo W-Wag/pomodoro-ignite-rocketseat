c90d8478295b11ed5db339c4ea53ae854d440eac
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2023-11-22 17:00:09 -0300

    Ajustando o DefaultLayout e o Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyle } from './styles/global'
-import { Button } from './components/Button'
 import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
 
 export function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <ThemeProvider theme={defaultTheme}>
-        <Button variant="primary" />
-        <Button variant="secondary" />
-        <Button variant="success" />
-        <Button variant="danger" />
-
-        <GlobalStyle />
-      </ThemeProvider>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
