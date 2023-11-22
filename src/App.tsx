5578e4a91498345a45552c183071b95e8fe73018
Wagner De Oliveira Martins <wagner.oliv.m@gmail.com>
2023-11-22 16:01:53 -0300

    Aulas Implementado o styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { Button } from './components/Button'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <ThemeProvider theme={defaultTheme}>
+        <Button variant="primary" />
+        <Button variant="secondary" />
+        <Button variant="success" />
+        <Button variant="danger" />
+
+        <GlobalStyle />
+      </ThemeProvider>
     </div>
-  );
+  )
 }
